3a8ae8816874281f5f2ab7fba3c8df4cd426916d
mohammed-mlh <mallahimohammed1@gmail.com>
2026-04-21 18:28:33 +0100

    Refactor application configuration in bootstrap/app.php to use __DIR__ for path resolution, ensuring consistency in routing file paths.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,11 +6,11 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Auth\AuthenticationException;
 use Symfony\Component\HttpFoundation\Response;
 
-return Application::configure(basePath: dirname(_DIR_))
+return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: _DIR_.'/../routes/web.php',
-        api: _DIR_.'/../routes/api.php',
-        commands: _DIR_.'/../routes/console.php',
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
